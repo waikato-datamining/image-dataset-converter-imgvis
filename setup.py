39941e3f2fc1d03d84ff4667e81a30b46ad004dd
Peter Reutemann <fracpete@gmail.com>
2025-10-31 14:45:15 +1300

    prepared for release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "image_dataset_converter",
         "opencv_python",
     ],
-    version="0.0.5",
+    version="0.1.0",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     entry_points={
